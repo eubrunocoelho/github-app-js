9a3d26418a02faef61102fed5aefcd81efb18aab
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-01-26 11:17:17 -0300

    initial commit

---
+++ js/views/userView.js
@@ -0,0 +1,23 @@
+import { elements } from './base.js';
+
+export const renderUser = ({
+    avatar_url,
+    html_url,
+    public_repos,
+    followers,
+    following
+}) => {
+    const markup = `
+        <div class="profile-header">
+            <img src="${avatar_url}" alt="Avatar">
+            <a href="${html_url}" target="_blank">Visitar perfil</a>
+        </div>
+        <ul class="profile-list'>
+            <li>Respositórios: ${public_repos}</li>
+            <li>Seguidores: ${followers}</li>
+            <li>Seguindo: ${following}</li>
+        </ul>
+    `;
+
+    elements.profile.insertAdjacentHTML('afterbegin', markup);
+};
